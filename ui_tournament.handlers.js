f6e06ca03d148337caa044be9548cbb2c0e652d5
cbmob0329 <pb2matsuyamadance@gmail.com>
2026-02-25 15:37:37 +0900

    Update ui_tournament.handlers.js

--- ui_tournament.handlers.js
+++ ui_tournament.handlers.js
@@ -1140,3 +1140,8 @@ window.MOBBR.ui = window.MOBBR.ui || {};
   });
 
 })();
+
+// ✅ splitロード検知（app.js の [CHECK] ui_tournament split 用）
+window.MOBBR = window.MOBBR || {};
+window.MOBBR.ui = window.MOBBR.ui || {};
+window.MOBBR.ui._tournamentHandlers = true;
